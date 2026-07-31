219751d7e10aecc666e065538e82830b33b4928c
SHS <sinhyeonseo097@gmail.com>
2026-07-31 13:40:43 +0900

    Initial comit

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+    ],
+});
+
+client.once('ready', () => {
+    console.log(`${client.user.tag} 로그인 완료!`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
